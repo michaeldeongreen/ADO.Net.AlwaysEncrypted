4b98fe8e44468eb5df5ff19cd489f73603e5b8f9
michaeldeongreen <michaeldeongreen@gmail.com>
2018-01-27 19:43:55 -0600

    *First check-in

---
+++ src/ADO.Net.AlwaysEncrypted.ConsoleApp/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ADO.Net.AlwaysEncrypted.ConsoleApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string connectionString = "Data Source=(local); integrated Security=sspi;initial catalog=Development; Column Encryption Setting = Enabled";
+            string storedProcedure = "dbo.Company_Select";
+            DataSet dataSet = new DataSet("Companies");
+
+            using (SqlConnection connection = new SqlConnection(connectionString))
+            {
+
+                using (SqlCommand command = new SqlCommand(storedProcedure, connection))
+                {
+                    command.CommandType = System.Data.CommandType.StoredProcedure;
+                    command.Parameters.AddWithValue("@Name", "coin").Direction = System.Data.ParameterDirection.Input;
+
+                    using (SqlDataAdapter adapter = new SqlDataAdapter())
+                    {
+                        adapter.SelectCommand = command;
+                        adapter.Fill(dataSet);
+                    }  
+                }
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
